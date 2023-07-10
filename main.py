fa65811705c1c981512e7e0559ee423e5b80ddf7
roczhang <mr.roczhang@outlook.com>
2023-07-10 23:37:45 +0800

    genohub scrapy with ajax

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ if __name__ == '__main__':
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
     # execute(['scrapy','crawl','quotes', "-O quotes.json"])
     # execute(['scrapy','crawl','quotes_google'])
-    execute(['scrapy','crawl','quotes_pages', "-O quotes_pages.json"])
+    # execute(['scrapy','crawl','quotes_pages', "-O quotes_pages.json"])
+    execute(['scrapy','crawl','genohub', "-O genohub.json"])
